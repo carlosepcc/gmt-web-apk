6ad454a528d900b16e15d1e61261f6c98610f8c7
Jesus F. Vazquez Biltre <jesusfvb2@gmail.com>
2022-03-22 10:15:09 -0400

    Validaciones en el login

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:gmt/app/component/frame.dart';
 import 'package:gmt/app/page/error404.dart';
 import 'package:gmt/app/page/home.dart';
--- lib/app/component/input.dart
+++ lib/app/component/input.dart
@@ -17,17 +17,15 @@ class InputData {
 }
 
 class InputPassword extends HookWidget {
-  const InputPassword({
-    Key? key,
-    required this.controller,
-  }) : super(key: key);
+  InputPassword({Key? key, required this.controller}) : super(key: key);
+
   final ValueNotifier<InputData> controller;
+
+  final showPassword = useState<bool>(true);
+  final error = useState<InputError>(InputError(message: "No puede estar vacío"));
 
   @override
   Widget build(BuildContext context) {
-    var showPassword = useState<bool>(true);
-    var error = useState<InputError>(InputError(message: "No puede estar vacío"));
-
     return TextField(
       obscureText: showPassword.value,
       keyboardType: TextInputType.visiblePassword,
@@ -44,36 +42,53 @@ class InputPassword extends HookWidget {
               showPassword.value = !showPassword.value;
             }),
       ),
-      onChanged: (value) {
-        if (value.isEmpty) {
-          error.value = InputError(message: "No puede estar vacío");
-        } else {
-          error.value = InputError();
-        }
-        controller.value = InputData(value: value, error: error.value.isInError());
-      },
+      onChanged: validate,
     );
+  }
+
+  void validate(String value) {
+    if (value.isEmpty) {
+      error.value = InputError(message: "No puede estar vacío");
+    } else if (value.length < 4) {
+      error.value = InputError(message: "Tamaño no valido");
+    } else {
+      error.value = InputError();
+    }
+    controller.value = InputData(value: value, error: error.value.isInError());
   }
 }
 
 class InputUsername extends HookWidget {
-  const InputUsername({Key? key, required this.controller}) : super(key: key);
+  InputUsername({Key? key, required this.controller}) : super(key: key);
+
   final ValueNotifier<InputData> controller;
+
+  final error = useState<InputError>(InputError(message: "No puede estar vacío"));
 
   @override
   Widget build(BuildContext context) {
-    var error = useState<InputError>(InputError(message: "No puede estar vacío"));
-
     return TextFormField(
-        keyboardType: TextInputType.name,
-        decoration: InputDecoration(labelText: "Usuario", errorText: error.value.message),
-        onChanged: (value) {
-          if (value.isEmpty) {
-            error.value = InputError(message: "No puede estar vacío");
-          } else {
-            error.value = InputError();
-          }
-          controller.value = InputData(value: value, error: error.value.isInError());
-        });
+        keyboardType: TextInputType.name, decoration: InputDecoration(labelText: "Usuario", errorText: error.value.message), onChanged: validate);
+  }
+
+  void validate(String value) {
+    if (value.isEmpty) {
+      error.value = InputError(message: "No puede estar vacío");
+    } else if (_numbers.hasMatch(value[0])) {
+      error.value = InputError(message: "No puede comenzar con un numero");
+    } else if (_lettersUppercase.hasMatch(value)) {
+      error.value = InputError(message: "No puede contener mayúsculas");
+    } else if (value.length < 4) {
+      error.value = InputError(message: "Tamaño no valido");
+    } else if (_specialCharacters.hasMatch(value)) {
+      error.value = InputError(message: "Contiene letras no valido");
+    } else {
+      error.value = InputError();
+    }
+    controller.value = InputData(value: value, error: error.value.isInError());
   }
 }
+
+RegExp _lettersUppercase = RegExp("[A-Z]");
+RegExp _specialCharacters = RegExp("[!@#\$&*~. ]");
+RegExp _numbers = RegExp("[0-9]");
