408089c491c6b44c8a20e65f64df366d83494b12
Jesus F. Vazquez Biltre <jesusfvb2@gmail.com>
2022-03-27 14:32:13 -0400

    Conexión con Heroku

--- lib/app/helper/axios.dart
+++ lib/app/helper/axios.dart
@@ -1,17 +1,22 @@
+// ignore: avoid_web_libraries_in_flutter
+import "dart:html";
+
 import 'package:dio/dio.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Axios {
-  final Dio dio = Dio(BaseOptions(baseUrl: "http://localhost:8080"));
+  final String _url = window.location.hostname! == "localhost" ? "http://localhost:8080" : "https://jesusfvb-gmt.herokuapp.com";
+
+  get _dio => Dio(BaseOptions(baseUrl: _url));
 
   post({required String path, required dynamic data, bool authorization = true}) async {
     if (authorization) {
       final prefs = await SharedPreferences.getInstance();
       String? jwt = prefs.getString("jwt");
       if (jwt != null) {
-        return await dio.post(path, data: data, options: Options(headers: {"Authorization": jwt}));
+        return await _dio.post(path, data: data, options: Options(headers: {"Authorization": jwt}));
       }
     }
-    return await dio.post(path, data: data);
+    return await _dio.post(path, data: data);
   }
 }
